4d0a9334f4a63ffe6b6d5a3b7e0d6379038c6a01
Lucas Samuel F. S <68969699+Dev3lopmentM4chine@users.noreply.github.com>
2023-05-17 14:44:21 -0300

    fix: eslint config in project

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-slate-900 relative">
-      <h1 className="text-7xl text-zinc-200 top-2/4 left-2/4 absolute">Hello World!</h1>
+    <div className="relative h-screen w-screen bg-slate-900">
+      <h1 className="absolute left-2/4 top-2/4 text-7xl text-zinc-200">
+        Hello World!
+      </h1>
     </div>
-  );
+  )
 }
